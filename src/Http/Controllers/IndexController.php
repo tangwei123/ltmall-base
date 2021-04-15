0b1c8f115f8415730c3c9ce6b6469caa30c10a56
tangwei <jsdy_tw1519@163.com>
2021-04-15 13:05:40 +0800

    first commit

---
+++ src/Http/Controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ltmall\Base\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller {
+    public function index(){
+        echo "ltmall base indexcontroller ";
+    }
+}
---
+++ src/LtmallbaseProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ltmall\Base;
+
+use Illuminate\Support\ServiceProvider;
+
+class LtmallbaseProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
